c64abf46a921103837165ace85732cf6c34c3fb4
BunyodNaimov <naimov.bunyod2021@gmail.com>
2023-07-22 13:51:00 +0500

    start project day 5

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-92qwjxmwm0xg*)t=6h%84lsi%sdjg8#ugprpiqf#y+moe0#mcg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
