5d416e1d23690f4531e8b656308a22d649046ffc
BunyodNaimov <naimov.bunyod2021@gmail.com>
2023-07-22 13:33:00 +0500

    start project day 5

--- users/views.py
+++ users/views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import authenticate
 from django.contrib.auth.hashers import make_password
 from rest_framework import generics, status
-from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 
