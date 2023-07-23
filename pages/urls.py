9bc9c0293f7c32975b2614f194452d7be6957f52
BunyodNaimov <naimov.bunyod2021@gmail.com>
2023-07-23 16:01:27 +0500

    start project day 6

--- pages/urls.py
+++ pages/urls.py
@@ -6,8 +6,8 @@ from pages.views import PageListAPIVew, PostListCreateAPIView, UninterestingPage
 urlpatterns = [
     path('', PageListAPIVew.as_view(), name='page_list'),
     path('blocked/', UninterestingPagesList.as_view(), name='blocked-page-list'),
-    path('<int:pk>/block/', MarkPageUninterested.as_view(), name='block-page'),
-    path('<int:pk>/unblock/', UnmarkPageUninteresting.as_view(), name='unblocked-page'),
+    path('posts/<int:pk>/block/', MarkPageUninterested.as_view(), name='block-page'),
+    path('posts/<int:pk>/unblock/', UnmarkPageUninteresting.as_view(), name='unblocked-page'),
     path('posts/', PostListCreateAPIView.as_view(), name='post-list-create'),
     path('posts/<int:pk>/like-dislike/', PostLikeAPIView.as_view(), name='post_like'),
     path('posts/<int:post_id>/comments/', CommentListCreateAPIView.as_view(), name='comment-list-create'),
